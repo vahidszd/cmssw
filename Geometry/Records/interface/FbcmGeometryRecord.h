774e79281af156e57e16ab9e5a307fe780973c93
Mohammad Sedghi <msedghi@cern.ch>
2021-01-14 23:53:48 +0100

    Fixed for first successful compilation in CMSSW_11_2_X

--- Geometry/Records/interface/FbcmGeometryRecord.h
+++ Geometry/Records/interface/FbcmGeometryRecord.h
@@ -9,7 +9,8 @@
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
-#include "boost/mpl/vector.hpp"
+//#include "boost/mpl/vector.hpp"
+#include "FWCore/Utilities/interface/mplVector.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/Records/interface/GeometryFileRcd.h"
 #include "CondFormats/AlignmentRecord/interface/GlobalPositionRcd.h"
@@ -18,7 +19,7 @@
 class FbcmGeometryRecord 
 : public edm::eventsetup::DependentRecordImplementation<
 				FbcmGeometryRecord,
-				boost::mpl::vector<	IdealGeometryRecord,
+				edm::mpl::Vector<	IdealGeometryRecord,
 									GeometryFileRcd, 
 									GlobalPositionRcd> > {};
 
