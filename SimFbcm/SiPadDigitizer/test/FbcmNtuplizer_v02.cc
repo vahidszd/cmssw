a2f9ee542d4f0241b6c1577013b293f5cc1414cf
Mohammad Sedghi <msedghi@cern.ch>
2021-02-15 07:28:13 +0100

    Backup FbcmNtuplizer_v2 as _v02

--- SimFbcm/SiPadDigitizer/test/FbcmNtuplizer_v02.cc
+++ SimFbcm/SiPadDigitizer/test/FbcmNtuplizer_v02.cc
@@ -21,19 +21,21 @@
 
 #include "TTree.h"
 #include <string> 
+#include <cmath>
 
 #define MAX_SIMHIT_SIZE 100
 #define MAX_DIGHIT_SIZE 100
 #define AREA_GROUP_LEN 8
+#define NUMOFDIGIBXs 3
 
 #define TEST_IND 7
 
 using namespace std;
 
-class FbcmNtuplizer_v2 : public edm::EDAnalyzer {
+class FbcmNtuplizer_v02 : public edm::EDAnalyzer {
 public:
-  explicit FbcmNtuplizer_v2(const edm::ParameterSet&);
-  ~FbcmNtuplizer_v2();
+  explicit FbcmNtuplizer_v02(const edm::ParameterSet&);
+  ~FbcmNtuplizer_v02();
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
@@ -74,9 +76,23 @@ private:
   int BxSlotCnt; 
   int nValidDigiToAs;
   int nValidDigiToTs;
+  int nDigiNonZeros;
+  int nDigiUncertains;
   
   int sum_nSimHitsPerEvnt[AREA_GROUP_LEN];
   int sum_nDigiHitsPerEvnt[AREA_GROUP_LEN];
+  
+  int Sim_M_PerEvent[AREA_GROUP_LEN]; // M=(nbrOfSenosrs * nbrOfBxPerEnt) // nbrOfBxPerEnt=7 in G4Sim
+  int Digi_M_PerEvent[AREA_GROUP_LEN]; // M=(nbrOfSenosrs * nbrOfBxPerEnt) // nbrOfBxPerEnt=NUMOFDIGIBXs in DIGI
+  // notice that the nbr of uncertain BXs should be subtracted from M
+  
+  
+  int nNonZerosPerEvnt[AREA_GROUP_LEN];
+  int nUncertainsPerEvnt[AREA_GROUP_LEN];
+  
+  
+  
+  float DigiLambdaZeroCounting[AREA_GROUP_LEN];
   
   float SimLambda[AREA_GROUP_LEN];
   float DigiLambda[AREA_GROUP_LEN];
@@ -91,8 +107,8 @@ private:
   float SimTofs[MAX_SIMHIT_SIZE];
   float SimTof_perBx[MAX_SIMHIT_SIZE];
 
-  int DigiHitStatus[3];
-  int nDigiHits[3];
+  int DigiHitStatus[NUMOFDIGIBXs];
+  int nDigiHits[NUMOFDIGIBXs];
   
   float DigiToAs[MAX_DIGHIT_SIZE] ;
   float DigiToTs[MAX_DIGHIT_SIZE];
@@ -133,7 +149,7 @@ private:
 //
 // constructors and destructor
 //
-FbcmNtuplizer_v2::FbcmNtuplizer_v2(const edm::ParameterSet& iConfig) :
+FbcmNtuplizer_v02::FbcmNtuplizer_v02(const edm::ParameterSet& iConfig) :
   TokenTag_(consumes< edm::DetSetVector<SiPadDigiData> >(iConfig.getParameter<edm::InputTag>("FbcmDigiTag")))
 {
   edm::Service<TFileService> fs;
@@ -153,7 +169,10 @@ FbcmNtuplizer_v2::FbcmNtuplizer_v2(const edm::ParameterSet& iConfig) :
   FixedValuesTree->Branch("EachSensorAreaVect" , EachSensorAreaVect , "EachSensorAreaVect[8]/F" );
   FixedValuesTree->Branch("nbrOfSenosrsVect" , nbrOfSenosrsVect , "nbrOfSenosrsVect[8]/I" );
   FixedValuesTree->Branch("TotalSensAreaPerGroup" , TotalSensAreaPerGroup , "TotalSensAreaPerGroup[8]/F" );
-		
+  FixedValuesTree->Branch("Sim_M_PerEvent" , Sim_M_PerEvent , "Sim_M_PerEvent[8]/I" );
+  FixedValuesTree->Branch("Digi_M_PerEvent" , Digi_M_PerEvent , "Digi_M_PerEvent[8]/I" );
+  
+  
   Int_t bsize = 32000; //default
 
   for (int i = 0 ; i < 8 ; i++) {
@@ -192,6 +211,14 @@ Flux_Tree->Branch("SimFlux" , SimFlux , "SimFlux[8]/F" );
 Flux_Tree->Branch("DigiFlux" , DigiFlux , "DigiFlux[8]/F" ); 
 
 
+  int nNonZerosPerEvnt[AREA_GROUP_LEN];
+  int nUncertainsPerEvnt[AREA_GROUP_LEN];
+Flux_Tree->Branch("nNonZerosPerEvnt" , nNonZerosPerEvnt , "nNonZerosPerEvnt[8]/I" );
+Flux_Tree->Branch("nUncertainsPerEvnt" , nUncertainsPerEvnt , "nUncertainsPerEvnt[8]/I" );
+//Flux_Tree->Branch("SimLambda" , SimLambda , "SimLambda[8]/F" );
+Flux_Tree->Branch("DigiLambdaZeroCounting" , DigiLambdaZeroCounting , "DigiLambdaZeroCounting[8]/F" );
+
+
 
 /*
   theTree->Branch("SensorRho" , &sensorRho , "SensorRho/f[8,22]" );
@@ -210,7 +237,7 @@ Flux_Tree->Branch("DigiFlux" , DigiFlux , "DigiFlux[8]/F" );
 }
 
 
-FbcmNtuplizer_v2::~FbcmNtuplizer_v2()
+FbcmNtuplizer_v02::~FbcmNtuplizer_v02()
 {
  
   // do anything here that needs to be done at desctruction time
@@ -225,7 +252,7 @@ FbcmNtuplizer_v2::~FbcmNtuplizer_v2()
 
 // ------------ method called for each event  ------------
 void
-FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+FbcmNtuplizer_v02::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
   using namespace edm;
@@ -233,9 +260,19 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   edm::Handle< edm::DetSetVector<SiPadDigiData> > handle;
   iEvent.getByToken(TokenTag_,handle);
   
+  int tmp_nZeros;
+  int nZerosPerEvntTMP[AREA_GROUP_LEN]; 
+int nZerosPerEvnt[AREA_GROUP_LEN];
+int cnt=0;
+  
+  
   for (int i=0 ; i< AREA_GROUP_LEN ; i++) {
   sum_nSimHitsPerEvnt[i] = 0;
   sum_nDigiHitsPerEvnt[i] = 0;
+  nNonZerosPerEvnt[i] = 0;
+  nUncertainsPerEvnt[i] = 0; 
+  
+  nZerosPerEvntTMP[i] = 0;
   }
   
   int SiDieId;
@@ -251,6 +288,7 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
     for (std::vector<SiPadDigiData>::const_iterator Digidata_it = itDetSet->begin() ; Digidata_it < itDetSet->end() ; ++Digidata_it) {
       //const SiPadDigiData Digidata= *(Digidata_it);
       //std::cout << *(Digidata_it) ;
+	  cnt++;
 	  
 	  SiDieId=Digidata_it->SiliconDieIndex() ;
 	  SensorGroupIndex = SiDieId % nbrOfDiesPerRing;
@@ -284,13 +322,22 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 	  
 	  sum_nSimHitsPerEvnt[SensorGroupIndex] += nSimParticles;
 	  
-	  nValidDigiToAs=0;
-	  nValidDigiToTs=0;
+	  nValidDigiToAs = 0;
+	  nValidDigiToTs = 0;
+	  nDigiNonZeros = 0 ;
+	  nDigiUncertains = 0;
+		
+	  tmp_nZeros =0;
+	  
 	  BxSlotCnt=0; // just to make sure starting from non-zero index if in the case of using BxSlotNo < 0. 
       for(auto hit : Digidata_it->BxSlotHitAnalysisVector() ) {	  
 		DigiHitStatus[BxSlotCnt] = hit.Bx_HitStatusInt();
 		nDigiHits[BxSlotCnt] = hit.nbrOfRecognizedHitsInBx(); 
 		BxSlotCnt++;
+		
+		nDigiNonZeros += (int)(hit.Bx_HitStatusInt() == HitStatus::NonZero);
+		nDigiUncertains += (int)(hit.Bx_HitStatusInt() == HitStatus::Uncertain); 
+		tmp_nZeros += (int)(hit.Bx_HitStatusInt() == HitStatus::Zero);
 		
 		for (auto ToaTot : hit.TotToaVectort()) {
 			if ( (ToaTot.ToAToAStatusInt() == ToAStatus::FullyWithinBx || ToaTot.ToAToAStatusInt() == ToAStatus::WithinBx_LastsAfter ) ) {
@@ -303,8 +350,9 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 		
       }
 	  sum_nDigiHitsPerEvnt[SensorGroupIndex] += nValidDigiToAs;
-
-
+	  nNonZerosPerEvnt[SensorGroupIndex] += nDigiNonZeros;
+	  nUncertainsPerEvnt[SensorGroupIndex] += nDigiUncertains;
+	  nZerosPerEvntTMP[SensorGroupIndex] += tmp_nZeros;
       //theTree->Fill();
 	  SizeTest_tree[SensorGroupIndex]->Fill();
       //std::cout << "detected raw ID: " << fbdetId.rawId() << "  : ";
@@ -316,14 +364,29 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 		
     //histo->Fill(itDetSet->detId());
   }
-  
+
+//std::cout << "Digi_M          nNonZeros       nUncertains  nZerosTMP  SumZeroUncertNonZero   nZeroOrg "  << cnt << " \n";
+
   for (int i =0 ; i < AREA_GROUP_LEN ; i++) {
-	SimLambda[i] = (float)sum_nSimHitsPerEvnt[i]/nbrOfSenosrsVect[i]/7.0;
-    DigiLambda[i] = (float)sum_nDigiHitsPerEvnt[i]/nbrOfSenosrsVect[i]/3.0;
+	SimLambda[i] = (float)sum_nSimHitsPerEvnt[i]/Sim_M_PerEvent[i];
+    DigiLambda[i] = (float)sum_nDigiHitsPerEvnt[i]/(Digi_M_PerEvent[i]-nUncertainsPerEvnt[i]);
     SimFlux[i] = (float) sum_nSimHitsPerEvnt[i]/TotalSensAreaPerGroup[i]/7.0; // devided by 7 due to 7 bunch crossings
-    DigiFlux[i] = (float) sum_nDigiHitsPerEvnt[i]/TotalSensAreaPerGroup[i]/3.0; // devided by 3 because we had consdered only 3 BXs, i.e. {0,1,2},  in the Digi
-	
+    DigiFlux[i] = (float) sum_nDigiHitsPerEvnt[i]/TotalSensAreaPerGroup[i]/((float)NUMOFDIGIBXs); // devided by 3 because we had consdered only 3 BXs, i.e. {0,1,2},  in the Digi
+	//-------------
+	nZerosPerEvnt[i] = ( Digi_M_PerEvent[i] - nUncertainsPerEvnt[i] ) - nNonZerosPerEvnt[i]; 
+	//nNonZerosPerEvnt[SensorGroupIndex] += nDigiNonZeros;
+    //nUncertainsPerEvnt[SensorGroupIndex] += nDigiUncertains;
+	int New_Digi_M_i = (Digi_M_PerEvent[i]-nUncertainsPerEvnt[i]) ;
+	if ( nZerosPerEvnt[i] > 0 &&  New_Digi_M_i > 0 ) {
+		DigiLambdaZeroCounting[i] = -log2( (float)nZerosPerEvnt[i]/ New_Digi_M_i );
+	}
+	else {
+		throw cms::Exception("Fetal error in counting Zeros") << "a Negetative or zero value found for the number of Zeors per Event, May be the Zero-Starvation occured!"; 
+	}
+		
+		//std::cout << Digi_M_PerEvent[i] << "\t" << nNonZerosPerEvnt[i] << "\t" << nUncertainsPerEvnt[i]  << "\t" << nZerosPerEvntTMP[i] <<"\t"<< nZerosPerEvntTMP[i] + nUncertainsPerEvnt[i] + nNonZerosPerEvnt[i] << "\t" << nZerosPerEvnt[i] <<"\n";
   }
+  //std::cout << "\n";
   
   Flux_Tree->Fill(); 
 
@@ -332,20 +395,20 @@ FbcmNtuplizer_v2::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-FbcmNtuplizer_v2::beginJob()
+FbcmNtuplizer_v02::beginJob()
 {
   
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-FbcmNtuplizer_v2::endJob() 
+FbcmNtuplizer_v02::endJob() 
 {
 }
 
 // ------------ method called when starting to processes a run  ------------
 
-  void FbcmNtuplizer_v2::beginRun(edm::Run const&, edm::EventSetup const& iSetup) {
+  void FbcmNtuplizer_v02::beginRun(edm::Run const&, edm::EventSetup const& iSetup) {
 	  iSetup.get<FbcmGeometryRecord>().get(theFbcmGeom); 
 	  const std::vector<const FbcmStationGeom*> AllStatitons = theFbcmGeom->Stations();
 	  int NbrOfStations = theFbcmGeom->NumOfStations()*2; // NumOfStations() just reports per end
@@ -378,12 +441,18 @@ FbcmNtuplizer_v2::endJob()
 		//std::cout << EachSensorAreaVect[i] << " \t";
 		//std::cout << TotalSensAreaPerGroup[i] << " \n";
 	}
-	 	
+	
+	for (int i=0 ; i < AREA_GROUP_LEN ; i++) {
+		// M=(nbrOfSenosrs * nbrOfBxPerEnt) 
+		Sim_M_PerEvent[i] = nbrOfSenosrsVect[i] * 7; // nbrOfBxPerEnt=7 in G4Sim
+		Digi_M_PerEvent[i] = nbrOfSenosrsVect[i] * NUMOFDIGIBXs; 
+	}
+	
 	FixedValuesTree->Fill();
   
   }
 
-void FbcmNtuplizer_v2::ZeroInitArrays(){
+void FbcmNtuplizer_v02::ZeroInitArrays(){
 
 	for (int i=0 ; i< AREA_GROUP_LEN ; i++) {
 	nbrOfSenosrsVect[i]=0;
@@ -395,7 +464,7 @@ void FbcmNtuplizer_v2::ZeroInitArrays(){
 // ------------ method called when ending the processing of a run  ------------
 /*
   void 
-  FbcmNtuplizer_v2::endRun(edm::Run const&, edm::EventSetup const&)
+  FbcmNtuplizer_v02::endRun(edm::Run const&, edm::EventSetup const&)
   {
   }
 */
@@ -403,7 +472,7 @@ void FbcmNtuplizer_v2::ZeroInitArrays(){
 // ------------ method called when starting to processes a luminosity block  ------------
 /*
   void 
-  FbcmNtuplizer_v2::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+  FbcmNtuplizer_v02::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
   {
   }
 */
@@ -411,14 +480,14 @@ void FbcmNtuplizer_v2::ZeroInitArrays(){
 // ------------ method called when ending the processing of a luminosity block  ------------
 /*
   void 
-  FbcmNtuplizer_v2::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+  FbcmNtuplizer_v02::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
   {
   }
 */
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
-FbcmNtuplizer_v2::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+FbcmNtuplizer_v02::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   //The following says we do not know what parameters are allowed so do no validation
   // Please change this to state exactly what you do use, even if it is no parameters
   //  edm::ParameterSetDescription desc;
@@ -427,4 +496,4 @@ FbcmNtuplizer_v2::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(FbcmNtuplizer_v2);
+DEFINE_FWK_MODULE(FbcmNtuplizer_v02);
